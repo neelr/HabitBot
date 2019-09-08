cfb3dde1d357f8c0b2d4c3c8be0d34588ac148ca
Neel Redkar <neel.redkar@outlook.com>
2019-09-08 16:39:45 -0700

    update app to backend

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import {AsyncStorage, ScrollView, StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { Icon } from "react-native-elements";
 import { GiftedChat } from 'react-native-gifted-chat';
@@ -9,7 +9,11 @@ const colors = {
 	secondary: "#08090A",
 	third: "#F4FAFF"
 }
-x = 1
+
+async function clear () {
+	await AsyncStorage.clear();
+}
+clear();
 class Home extends React.Component {
 	static navigationOptions = {
 		title: "Home"
@@ -44,48 +48,73 @@ class Help extends React.Component {
 	}
 }
 class Chat extends React.Component {
+	ID () {
+		return '_' + Math.random().toString(36).substr(2, 9);
+	}
 	static navigationOptions = {
 		title: "Chat"
 	}
 	state = {
 		messages: [],
+		key:0,
 	}
-
-	componentWillMount() {
-		this.setState({
-			messages: [
-				{
-					_id:1,
-					text: 'ik you are addicted',
-					user: {
-						_id: 2,
-						name: 'AdicBot',
-						avatar: 'https://avatars3.githubusercontent.com/u/35831013?s=460&v=4',
-					},
-				},
-			],
+	async componentWillMount() {
+		await AsyncStorage.getItem("logs",(err,res)=> {
+			if (res) {
+				this.setState({
+					messages: JSON.parse(res)
+				})
+			} else {
+				this.setState({
+					messages: [
+						{
+							_id:this.ID(),
+							text: 'Hi! Im an Adiction Bot!',
+							user: {
+								_id: 2,
+								name: 'AdicBot',
+								avatar: 'http://michaelkonik.com/wp-content/uploads/2010/11/no20drugs1.jpg',
+							},
+						},
+					],
+				})
+			}
 		})
 	}
 
 	onSend(messages = []) {
+		console.log(messages);
 		messages[0].createdAt = null;
 		this.setState(previousState => ({
 			messages: GiftedChat.append(previousState.messages, messages),
 		}))
-		botmes = [{
-			_id:x,
-			text:"YO I DONT KNOW WHAT YOU ARE SAYING IDIOT",
-			user: {
-				_id: 2,
-				name: 'AdicBot',
-				avatar: 'https://avatars3.githubusercontent.com/u/35831013?s=460&v=4',
-			}
-		}]
-		this.setState(previousState => ({
-			messages: GiftedChat.append(previousState.messages, botmes),
-		}))
-		console.log(messages)
-		x++;
+		fetch("https://adicbot.glitch.me/comp",{
+			headers: {
+				'Content-Type': 'application/json',
+			  },
+			method:"POST",
+			body: JSON.stringify({
+				message:messages[0].text
+			  }),
+		})
+			.then((res)=>res.json())
+			.then(async (res)=> {
+				console.log(res)
+				this.setState(previousState => ({
+					messages: GiftedChat.append(previousState.messages, [
+						{
+							_id:this.ID(),
+							text: res.response,
+							user: {
+								_id: 2,
+								name: 'AdicBot',
+								avatar: 'http://michaelkonik.com/wp-content/uploads/2010/11/no20drugs1.jpg',
+							},
+						},
+					]),
+				}))
+				await AsyncStorage.setItem("logs",JSON.stringify(this.state.messages));
+			})
 	}
 	render() {
 		return (
